17ee0562038a44903adb7b536812598c87340358
kofkdk <kofkdk@users.noreply.github.com>
2026-05-20 20:30:26 +0700

    art: replace SVG with original pixel art PNG sprites (Pillow generated)

--- src/utils/sprites.js
+++ src/utils/sprites.js
@@ -1,77 +1,59 @@
-// Sprite loader - imports all SVG assets
+// Sprite loader - imports all PNG pixel art assets
 // Fish sprites
-import goldfish from '../assets/sprites/goldfish.svg';
-import carp from '../assets/sprites/carp.svg';
-import catfish from '../assets/sprites/catfish.svg';
-import tilapia from '../assets/sprites/tilapia.svg';
-import guppy from '../assets/sprites/guppy.svg';
-import salmon from '../assets/sprites/salmon.svg';
-import bass from '../assets/sprites/bass.svg';
-import trout from '../assets/sprites/trout.svg';
-import pike from '../assets/sprites/pike.svg';
-import tuna from '../assets/sprites/tuna.svg';
-import marlin from '../assets/sprites/marlin.svg';
-import swordfish from '../assets/sprites/swordfish.svg';
-import pufferfish from '../assets/sprites/pufferfish.svg';
-import shark from '../assets/sprites/shark.svg';
-import whale from '../assets/sprites/whale.svg';
-import squid from '../assets/sprites/squid.svg';
-import manta from '../assets/sprites/manta.svg';
-import dragon from '../assets/sprites/dragon.svg';
-import kraken from '../assets/sprites/kraken.svg';
-import phoenix from '../assets/sprites/phoenix.svg';
-import leviathan from '../assets/sprites/leviathan.svg';
-import cosmic from '../assets/sprites/cosmic.svg';
-import ancient from '../assets/sprites/ancient.svg';
+import goldfish from '../assets/sprites/goldfish.png';
+import carp from '../assets/sprites/carp.png';
+import catfish from '../assets/sprites/catfish.png';
+import tilapia from '../assets/sprites/tilapia.png';
+import guppy from '../assets/sprites/guppy.png';
+import salmon from '../assets/sprites/salmon.png';
+import bass from '../assets/sprites/bass.png';
+import trout from '../assets/sprites/trout.png';
+import pike from '../assets/sprites/pike.png';
+import tuna from '../assets/sprites/tuna.png';
+import marlin from '../assets/sprites/marlin.png';
+import swordfish from '../assets/sprites/swordfish.png';
+import pufferfish from '../assets/sprites/pufferfish.png';
+import shark from '../assets/sprites/shark.png';
+import whale from '../assets/sprites/whale.png';
+import squid from '../assets/sprites/squid.png';
+import manta from '../assets/sprites/manta.png';
+import dragon from '../assets/sprites/dragon.png';
+import kraken from '../assets/sprites/kraken.png';
+import phoenix from '../assets/sprites/phoenix.png';
+import leviathan from '../assets/sprites/leviathan.png';
+import cosmic from '../assets/sprites/cosmic.png';
+import ancient from '../assets/sprites/ancient.png';
 
 // Hybrid sprites
-import hybrid_goldentrout from '../assets/sprites/hybrid_goldentrout.svg';
-import hybrid_seabass from '../assets/sprites/hybrid_seabass.svg';
-import hybrid_dragonshark from '../assets/sprites/hybrid_dragonshark.svg';
-import hybrid_rainbow from '../assets/sprites/hybrid_rainbow.svg';
-import hybrid_abyssal from '../assets/sprites/hybrid_abyssal.svg';
-import hybrid_celestial from '../assets/sprites/hybrid_celestial.svg';
-import hybrid_oceanlord from '../assets/sprites/hybrid_oceanlord.svg';
+import hybrid_goldentrout from '../assets/sprites/hybrid_goldentrout.png';
+import hybrid_seabass from '../assets/sprites/hybrid_seabass.png';
+import hybrid_dragonshark from '../assets/sprites/hybrid_dragonshark.png';
+import hybrid_rainbow from '../assets/sprites/hybrid_rainbow.png';
+import hybrid_abyssal from '../assets/sprites/hybrid_abyssal.png';
+import hybrid_celestial from '../assets/sprites/hybrid_celestial.png';
+import hybrid_oceanlord from '../assets/sprites/hybrid_oceanlord.png';
 
-// Environment
-import boat from '../assets/sprites/boat.svg';
+// Boat
+import boat from '../assets/sprites/boat.png';
 
-// UI
-import coinIcon from '../assets/ui/coin.svg';
-import gemIcon from '../assets/ui/gem.svg';
-import energyIcon from '../assets/ui/energy.svg';
-
-export const SPRITES = {
-  fish: {
-    goldfish, carp, catfish, tilapia, guppy,
-    salmon, bass, trout, pike,
-    tuna, marlin, swordfish, pufferfish,
-    shark, whale, squid, manta,
-    dragon, kraken, phoenix, leviathan,
-    cosmic, ancient
-  },
-  hybrid: {
-    goldentrout: hybrid_goldentrout,
-    seabass: hybrid_seabass,
-    dragonshark: hybrid_dragonshark,
-    rainbowfish: hybrid_rainbow,
-    abyssal: hybrid_abyssal,
-    celestial: hybrid_celestial,
-    oceanlord: hybrid_oceanlord
-  },
-  environment: { boat },
-  ui: { coin: coinIcon, gem: gemIcon, energy: energyIcon }
+const SPRITES = {
+  goldfish, carp, catfish, tilapia, guppy,
+  salmon, bass, trout, pike,
+  tuna, marlin, swordfish, pufferfish,
+  shark, whale, squid, manta,
+  dragon, kraken, phoenix, leviathan,
+  cosmic, ancient,
+  hybrid_goldentrout, hybrid_seabass, hybrid_dragonshark,
+  hybrid_rainbow, hybrid_abyssal, hybrid_celestial, hybrid_oceanlord,
+  boat
 };
 
-export function getFishSprite(fishId) {
-  // Check hybrids first
-  if (SPRITES.hybrid[fishId]) return SPRITES.hybrid[fishId];
-  // Then regular fish
-  if (SPRITES.fish[fishId]) return SPRITES.fish[fishId];
-  // Fallback
-  return SPRITES.fish.goldfish;
+export function getFishSprite(id) {
+  return SPRITES[id] || SPRITES.goldfish;
 }
 
-export function getUIIcon(name) {
-  return SPRITES.ui[name] || '';
+export function getBoatSprite() {
+  return SPRITES.boat;
 }
+
+export default SPRITES;
